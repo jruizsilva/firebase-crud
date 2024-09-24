3e086815f0adb6474ab1f088240602ecd44f4522
Jonathan Ruiz Silva <ruizsilvajonathan@gmail.com>
2024-09-24 14:25:20 -0300

    render home screen conditionally based on platform

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -3,6 +3,8 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { HomeScreen } from './screens/stackScreens/HomeScreen'
 import { PaperProvider } from 'react-native-paper'
 import { CombinedDefaultTheme } from './themes'
+import { Platform } from 'react-native'
+import { HomeWebScreen } from './screens/stackScreens/HomeWebScreen'
 
 export type RootStackParamList = {
   Home: undefined
@@ -16,7 +18,10 @@ export function Navigation() {
     <PaperProvider theme={CombinedDefaultTheme}>
       <NavigationContainer theme={CombinedDefaultTheme}>
         <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen
+            name="Home"
+            component={Platform.OS === 'web' ? HomeWebScreen : HomeScreen}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </PaperProvider>
---
+++ src/screens/stackScreens/HomeWebScreen.tsx
@@ -0,0 +1,42 @@
+import * as RN from 'react-native'
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
+import { RootStackParamList } from '../../Navigation'
+import { Button, Text } from 'react-native-paper'
+import { useCallback, useMemo, useRef, useState } from 'react'
+import BottomSheet from '@gorhom/bottom-sheet'
+import { BottomSheetView } from '@gorhom/bottom-sheet'
+
+export function HomeWebScreen({
+  navigation,
+  route,
+}: NativeStackScreenProps<RootStackParamList, 'Home'>) {
+  const sheetRef = useRef<BottomSheet>(null)
+  const [isOpen, setIsOpen] = useState(true)
+
+  const snapPoints = useMemo(() => ['25%', '50%'], [])
+
+  const handleSnapPress = useCallback((index: number) => {
+    sheetRef.current?.snapToIndex(index)
+    sheetRef.current?.expand()
+    setIsOpen(true)
+  }, [])
+
+  return (
+    <RN.View style={{ flexGrow: 1 }}>
+      <Text>Home Web</Text>
+      <Button style={{ marginTop: 30 }} onPress={() => handleSnapPress(0)}>
+        Show
+      </Button>
+      <BottomSheet
+        ref={sheetRef}
+        snapPoints={snapPoints}
+        enablePanDownToClose={true}
+        onClose={() => setIsOpen(false)}
+      >
+        <BottomSheetView>
+          <Text>Hello</Text>
+        </BottomSheetView>
+      </BottomSheet>
+    </RN.View>
+  )
+}
